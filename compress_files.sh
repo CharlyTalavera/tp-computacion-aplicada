5ff6c65b6c838130e318cad95db4953c3871e013
root <root@TPServer.up>
2025-05-20 22:59:35 -0300

    compress www_dir and backup_dir folders

--- compress_files.sh
+++ compress_files.sh
@@ -5,4 +5,6 @@ tar --exclude=/proc/kcore czf proc.tar.gz /proc
 tar czf etc.tar.gz /etc
 tar czf opt.tar.gz /opt
 tar czf root.tar.gz /root
+tar czf www_dir.tar.gz /www_dir
+tar czf backup_dir.tar.gz /backup_dir
 tar czf - /var/ | split -b 49M - var.tar.gz.part.
