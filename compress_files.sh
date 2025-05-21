2b09aece5a5c73978195d9f6da3eea7d27405f0b
root <root@TPServer.up>
2025-05-20 23:13:38 -0300

    update compress file script

--- compress_files.sh
+++ compress_files.sh
@@ -1,10 +1,10 @@
-!#/bin/bash
+#!/bin/bash
 
 # Ignoramos /proc/kcore, caso contrario al intentar comprimir demora mucho tiempo
-tar --exclude=/proc/kcore czf proc.tar.gz /proc
-tar czf etc.tar.gz /etc
-tar czf opt.tar.gz /opt
-tar czf root.tar.gz /root
-tar czf www_dir.tar.gz /www_dir
-tar czf backup_dir.tar.gz /backup_dir
-tar czf - /var/ | split -b 49M - var.tar.gz.part.
+tar --exclude=/proc/kcore -czf proc.tar.gz /proc
+tar -czf etc.tar.gz /etc
+tar -czf opt.tar.gz /opt
+tar -czf root.tar.gz /root
+tar -czf www_dir.tar.gz /www_dir
+tar -czf backup_dir.tar.gz /backup_dir
+tar -czf - /var/ | split -b 49M - var.tar.gz.part.
